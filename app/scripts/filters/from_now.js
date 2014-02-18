84804158dc01f887500c064f3dd8dc0e90db3304
Matt Van Veenendaal <m@mattvv.com>
2014-02-18 13:12:20 -0800

    Build: 1 Branch: master  0.0.0 SHA: 55bdf5a

--- app/scripts/filters/from_now.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/*global moment:false */
-
-angular.module('linepleaseApp').
-  filter('fromNow', function() {
-    return function(dateString) {
-      return moment(new Date(dateString)).fromNow();
-    };
-  });
